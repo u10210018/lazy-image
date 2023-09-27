ce1c8c2b58f43a02813b13e592795717ff101a22
u10210018 <69283233+u10210018@users.noreply.github.com>
2023-09-27 22:50:00 +0800

    add lib setting

---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+import LazyImage from './LazyImage.vue'
+
+export { LazyImage }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue'],
+      dts: './auto-imports.d.ts',
+      injectAtEnd: true,
+    }),
+    cssInjectedByJsPlugin({ styleId: 'u10210018_lazy_image' }),
+    dts({ include: ['lib'], rollupTypes: true }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@lib': resolve(__dirname, 'lib'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      name: 'LazyImage',
+      fileName: 'lazy-image',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
